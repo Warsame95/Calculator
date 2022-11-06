bebf7bfcb680bdaf40bc8bfdba91c804eeed18a8
Warsame <warsameahmed95@gmail.com>
2022-11-06 21:39:46 +0000

    numbers on calculator now populate the display when clicked

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Add display function to all digits
+const digits = document.getElementsByClassName("digits");
+Array.from(digits).forEach((digit) => {digit.addEventListener('click',display)});
+
+let displayArea = document.getElementById('display');
+
 function add(op1,op2){
     return op1 + op2;
 }
@@ -25,5 +31,14 @@ function operate(operator, op1, op2){
         case "/":
             return divide(op1,op2);
         
+    }
+}
+
+function display(e){
+    let num = e.target.innerText;
+    
+    //condition to prevent number overflowing
+    if(displayArea.innerText.length <= 7){
+        displayArea.innerText+=num;
     }
 }
